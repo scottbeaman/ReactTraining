9e826fdd2e5f40a2e9addb99a5c1e4f8df750348
Scott Beaman <scottbeaman1@gmail.com>
2017-01-12 11:00:38 +0000

    change text for home page button

--- app/components/Home.js
+++ app/components/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <h1>Github Battle</h1>
       <p className='lead'>What even is a jQuery?</p>
       <Link to='/playerOne'>
-        <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+        <button type='button' className='btn btn-lg btn-success'>Get Started with battle</button>
       </Link>
     </MainContainer>
   );
